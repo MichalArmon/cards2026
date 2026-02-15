16189d2eafe350cb78b0d9021877b6142dfa31db
MichalArmon <armonmic22003@gmail.com>
2026-02-15 13:20:23 +0200

    c

--- src/sandBox/components/CountryList.jsx
+++ src/sandBox/components/CountryList.jsx
@@ -18,66 +18,22 @@ import {
 } from "@mui/material";
 
 import React, { useEffect, useState } from "react";
+import useCountries from "../hooks/useCountries";
 const top = 55;
 
 export default function CountryList() {
-  const [countries, setCountries] = useState([]);
-  const [page, setPage] = useState(1);
-  const [rowPerPage, setRowPerPage] = useState(10);
-  const [sortOrder, setSortOrder] = useState("asc");
-  const [filteredCountries, setFilteredCountries] = useState([]);
+  const {
+    page,
+    rowPerPage,
+    countries,
+    filteredCountries,
+    sortOrder,
+    handleChange,
+    handleChangePage,
+    handleChangeRowForPage,
+    handleSortFirstName,
+  } = useCountries();
 
-  const handleChangeRowForPage = (event) => {
-    setRowPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-  const handleChangePage = (event, nextPage) => {
-    setPage(nextPage);
-  };
-
-  const getCountriesFromServer = async () => {
-    const response = await fetch(
-      "https://restcountries.com/v3.1/all?fields=name,currencies,capital,languages,cca2,flags",
-    );
-    const json = await response.json();
-    setCountries(json);
-    setFilteredCountries(json);
-    console.log(json);
-  };
-
-  useEffect(() => {
-    getCountriesFromServer();
-  }, []);
-
-  const handleSortFirstName = () => {
-    const sorted = [...countries].sort((a, b) => {
-      const nameA = a.name.official;
-      const nameB = b.name.official;
-      if (sortOrder === "asc") {
-        return nameA.localeCompare(nameB);
-      } else {
-        return nameB.localeCompare(nameA);
-      }
-    });
-    setCountries(sorted);
-    setFilteredCountries(sorted);
-    setPage(0);
-    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
-  };
-  const handleChange = (event) => {
-    const value = event.target.value.toLowerCase();
-    console.log(value);
-    setPage(0);
-    setFilteredCountries(
-      countries.filter((country) =>
-        country.name.official.toLowerCase().trim().includes(value),
-      ),
-    );
-  };
-
-  if (countries === 0) {
-    <Typography>NO Countries to show!</Typography>;
-  }
   return (
     <>
       {/* <Box>
